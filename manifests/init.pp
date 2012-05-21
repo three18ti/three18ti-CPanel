26e4fe7ac7120db8f2a0ee12753367e18e1e4d4f
three18ti <three18ti@gmail.com>
2012-05-21 17:32:15 -0600

    Updated init with current 'plan'

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,14 +13,39 @@
 # [Remember: No empty lines between comments and class definition]
 class CPanel {
 
-   yum_groupremove { yumgroup { '"group E-mail server"': } }
-   yum_groupremove { yumgroup { '"FTP server"': } }
-   yum_groupremove { yumgroup { '"KDE Desktop"': } }
-   yum_groupremove { yumgroup { '"Legacy X Window System compatibility"': } }
-   yum_groupremove { yumgroup { '"Web Server"': } }
-   yum_groupremove { yumgroup { '"Web Servlet Engine"': } }
-   yum_groupremove { yumgroup { '"Web-Based Enterprise Management"': } }
-   yum_groupremove { yumgroup { '"X Window System"': } }
+    # execute this first
+    file { 'config':
+        path    => '/etc/selinux/config',
+        ensure  => file,
+        require => File['cpanel'],
+        content => template("CPanel/selinux.config.erb"),
+    }
+
+    # then check if cpanel is installed
+    file { 'cpanel':
+        path    => '/etc/init.d/cpanel',
+        ensure  => file,
+    }
+
+    # if not, execute the following
+    {
+        # remove requisite packages
+        yum_groupremove { yumgroup { "mail-server": } }
+        yum_groupremove { yumgroup { "ftp-server": } }
+        yum_groupremove { yumgroup { "kde-desktop": } }
+        yum_groupremove { yumgroup { "legacy-x": } }
+        yum_groupremove { yumgroup { "web-server": } }
+        yum_groupremove { yumgroup { "web-servlet": } }
+        yum_groupremove { yumgroup { "system-management-wbem": } }
+        yum_groupremove { yumgroup { "x11": } }
+    
+        # disable firewall
+        exec => "chkconfig iptables off && service iptables stop",
+
+        # install cpanel
+        exec => "wget -O- http://httpupdate.cpanel.net/latest | sh -",
+
+    }
 
 
 
