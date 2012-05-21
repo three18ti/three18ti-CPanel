1e68cc245eb2b7b4d3dfe596ad3672c4d216e1f0
three18ti <three18ti@gmail.com>
2012-05-21 13:22:59 -0600

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: CPanel
+#
+# This module manages CPanel
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class CPanel {
+
+
+}
